36a7a912ef79b6675283b0596bd389c1dd0d8177
dkpinn <dkpinn@gmail.com>
2025-04-14 17:42:36 +0200

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,17 +1,14 @@
 PARSING_RULES = {
-    "ABSA_CHEQUE": {
-        "bank_name": "ABSA",
-        "account_type": "Cheque Account Statement",
-        "column_zones": {
-            "description": [95, 305],
-            "debit": [310, 390],
-            "credit": [395, 470],
-            "balance": [475, 999]
-        },
+    "ABSA_CHEQUE_ACCOUNT_STATEMENT": {
         "date_x_threshold": 95,
-        "multi_line_description": True,
-        "amount_comma_optional": True,
-        "balance_validation_passes": 5,
-        "negatives_have_dash": True
-    }
+        "column_zones": {
+            "description": (95, 305),
+            "debit": (310, 390),
+            "credit": (395, 470),
+            "balance": (475, 999)
+        }
+    },
+    # Add more configurations like:
+    # "ABSA_CREDIT_CARD_STATEMENT": {...},
+    # "STANDARD_BANK_CHEQUE_ACCOUNT_STATEMENT": {...},
 }
