174f5dfb0872e3939eb2397793b3ff8ce27408a4
dkpinn <dkpinn@gmail.com>
2025-04-14 17:57:51 +0200

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,3 +1,5 @@
+# rules.py
+
 PARSING_RULES = {
     "ABSA_CHEQUE_ACCOUNT_STATEMENT": {
         "date_x_threshold": 95,
@@ -6,8 +8,20 @@ PARSING_RULES = {
             "debit": (310, 390),
             "credit": (395, 470),
             "balance": (475, 999)
+        },
+        "amount_format": {
+            "thousands_separator": " ",
+            "decimal_separator": ".",
+            "negative_trailing": "Y"
+        },
+        "date_format": {
+            "formats": ["%d/%m/%Y", "%m %d"],
+            "year_optional": "Y"
+        },
+        "description": {
+            "multiline": true
         }
-    },
+    },  # <-- This comma was missing
     # Add more configurations like:
     # "ABSA_CREDIT_CARD_STATEMENT": {...},
     # "STANDARD_BANK_CHEQUE_ACCOUNT_STATEMENT": {...},
