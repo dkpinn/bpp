d4b0062ca459b9c0badcf5d0fe6fbce9bf475ead
dkpinn <dkpinn@gmail.com>
2025-04-15 19:25:21 +0200

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,13 +1,10 @@
-# rules.py
-
 PARSING_RULES = {
     "ABSA_CHEQUE_ACCOUNT_STATEMENT": {
-        "date_x_threshold": 95,
         "column_zones": {
-            "description": (85, 295),
-            "debit": (330, 450),
-            "credit": (455, 510),
-            "balance": (515, 999)
+            "description": (95, 305),
+            "debit": (310, 390),
+            "credit": (395, 470),
+            "balance": (475, 999)
         },
         "amount_format": {
             "thousands_separator": " ",
@@ -21,19 +18,6 @@ PARSING_RULES = {
         "description": {
             "multiline": True
         },
-        "multiline_description": True,
-        "parse_rules_applied": [
-            "x-coordinate zone-based classification",
-            "multiline description parsing",
-            "debit/credit/balance alignment checks",
-            "amount format normalization",
-            "balance delta validation",
-            "support for missing year in date",
-            "handling of trailing dash for negatives",
-            "remove amount/balance from description when wrongly included"
-        ]
-    },
-    # Add more configurations like:
-    # "ABSA_CREDIT_CARD_STATEMENT": {...},
-    # "STANDARD_BANK_CHEQUE_ACCOUNT_STATEMENT": {...},
+        "date_x_threshold": 95
+    }
 }
