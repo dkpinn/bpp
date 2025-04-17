65cefbf15c9df312122950002384ae03c9804528
dkpinn <dkpinn@gmail.com>
2025-04-17 11:37:48 +0200

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,3 +1,5 @@
+# rules.py
+
 PARSING_RULES = {
     "ABSA_CHEQUE_ACCOUNT_STATEMENT": {
         "column_zones": {
@@ -19,5 +21,26 @@ PARSING_RULES = {
             "multiline": True
         },
         "date_x_threshold": 95
+    },
+    "STANDARD_BANK_BUSINESS_CURRENT_ACCOUNT": {
+        "column_zones": {
+            "description": (0, 250),
+            "debit": (300, 370),
+            "credit": (375, 450),
+            "balance": (455, 999)
+        },
+        "amount_format": {
+            "thousands_separator": ".",
+            "decimal_separator": ",",
+            "negative_trailing": "Y"
+        },
+        "date_format": {
+            "formats": ["%m %d"],
+            "year_optional": "Y"
+        },
+        "description": {
+            "multiline": True
+        },
+        "date_x_threshold": 600
     }
 }
